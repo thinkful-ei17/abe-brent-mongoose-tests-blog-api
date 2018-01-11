9944876ec768823b32e42e1f4bfa2a48c42cc02d
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-01-10 18:46:05 -0800

    Add Additional tests

--- test/test-blog-app-integration.js
+++ test/test-blog-app-integration.js
@@ -11,15 +11,13 @@ const {TEST_DATABASE_URL} = require('../config');
 const {app, runServer, closeServer} = require('../server');
 const seedData = require('../seed-data.json');
 
-
-describe('GET /posts', function(){
+describe('Setup and TearDown', function(){
   before(function(){
     console.log('starting server');
     return runServer(TEST_DATABASE_URL);
   });
 
   beforeEach(function(){
-    console.log(seedData);
     return BlogPost.insertMany(seedData);
   });
 
@@ -33,32 +31,68 @@ describe('GET /posts', function(){
     return closeServer();
   });
 
+
+
+  describe('GET /posts', function(){
   /*
   Testing For:
   1. Count of Stories in Database should equal Request Totals on GET ALL
   2. It should return a post with the required fields
   3. id matches req.id
   */
-  let res;
-  it('should get all records and match database count', function(){
-    return BlogPost.count()
+    let res;
+    
+      it('should match request id with the post id', function(){
+      return BlogPost.find()
       .then(_res => {
         res = _res;
         return res;})
       .then(function(){
+      return chai.request(app)
+        .get(`/posts/${res[0].id}`);
+      })
+      .then(response => {
+          response.body.id.should.equal(res[0].id);
+        });
+          console.log(`Res.id has ${res[0].id} and response.body.id has ${response.body.id}`);
+          console.log(`Here is what we have in res: ${res[0].id}`);
+    });  
+
+
+    it('should get all records and match database count', function(){
         return chai.request(app)
-          .get('/posts');}) 
+      .get('/posts') 
       .then(response =>{
         response.should.have.status(200);
-        response.body.length.should.equal(res); 
+        response.body.length.should.equal(res.length); 
       }) 
       .catch(err => { console.log(err);});
   
+    });
+
+    it('should return a post with required fields', function(){
+      return chai.request(app)
+        .get('/posts')
+        .then(response =>{
+          response.body.forEach(function(story){
+            story.should.have.keys('title', 'content','author','id','created');
+          });
+        });
+    });
+
+
+
+
   });
 
-});
+  // describe('POST /posts', function(){
 
-// describe('POST Endpoints', function(){});
+  //   let res;
+  //   it('should');
+
+  // });
+
+});
 
 
 // describe('PUT Endpoints', function(){});
